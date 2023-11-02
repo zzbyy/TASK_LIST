adb33cbb06a0ac3ef0822a656bdbb2375f8f6eac
zz <zzbyy@live.com>
2023-11-02 16:13:45 +0800

    created TaskLsit view and task_list templated for todo app

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Task
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+class TaskList(generic.ListView):
+    model = Task
+    context_object_name = 'tasks'
